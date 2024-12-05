047c00e628f9a6297f4b3749c4703eb66023bd18
manisha4450 <chowdaryc226@gmail.com>
2024-12-05 16:25:38 +0530

    Add files via upload

---
+++ naturalnum1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class naturalnum1{
+    public static void main(String[] args){
+        int i=10;
+        while(i>0)
+        {
+            System.out.println(i);
+            i-=1;
+        }
+    }
+}
