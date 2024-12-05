535757d3d77ab0e5ffebbde071fdebeeff4026ad
manisha4450 <chowdaryc226@gmail.com>
2024-12-05 16:00:31 +0530

    Add files via upload

---
+++ decrement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class decrement{
+    public static void main(String[] args){
+        // post increment
+        int n=5;
+        System.out.println(n++);
+        //pre increment
+        int a=4;
+        System.out.println(++a);
+        //pre decrement
+        int b=6;
+        System.out.println(--b);
+        //post decrement
+        int a1=56;
+        System.out.println(a1--);
+    }
+}
